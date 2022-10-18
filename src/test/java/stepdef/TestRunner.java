fdc69ccdea662a54ea64bdd674bf1f883da07bcb
sreenidhimancha <sreenidhimancha95@gmail.com>
2022-10-18 09:52:31 +0530

    Project

---
+++ src/test/java/stepdef/TestRunner.java
@@ -0,0 +1,18 @@
+package stepdef;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features="src/test/java/features",
+		glue="stepdef" ,
+		tags= {"@sanity or @regression"}
+		
+		)
+
+public class TestRunner {
+
+}
